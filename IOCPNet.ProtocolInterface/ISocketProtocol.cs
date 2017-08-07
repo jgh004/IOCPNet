0802e1a5f9a16f7810145346c88cc3a16f9b3f8e
ITnmg <jgh004@163.com>
2017-08-07 10:51:21 +0800

    因socket类没有close方法,暂时注释掉.增加其他引用

---
+++ IOCPNet.ProtocolInterface/ISocketProtocol.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITnmg.IOCPNet.ProtocolInterface
+{
+	/// <summary>
+	/// 通信协议接口
+	/// </summary>
+    public interface ISocketProtocol
+	{
+		bool Encoder( byte[] src, ref byte[] dst, object packHead );
+		bool Decoder( ref byte[] waitBuffer, ref int waitLen, ref byte[] data, ref int dataLen, ref object packHead );
+	}
+}
---
+++ IOCPNet/SocketUserToken.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Sockets;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using ITnmg.IOCPNet.ProtocolInterface;
+
+namespace ITnmg.IOCPNet
+{
+	/// <summary>
+	/// 异步 socket 关联的用户程序对象
+	/// </summary>
+	public class SocketUserToken
+	{
+		/// <summary>
+		/// 缓存管理
+		/// </summary>
+		private BufferManager bufferManager;
+		
+
+		/// <summary>
+		/// 获取或设置唯一Id
+		/// </summary>
+		public Guid Id
+		{
+			get; set;
+		}
+
+		/// <summary>
+		/// 获取或设置当前 socket 连接
+		/// </summary>
+		public Socket CurrentSocket
+		{
+			get; set;
+		}
+
+		/// <summary>
+		/// 获取或设置发送数据用的SocketAsyncEventArgs
+		/// </summary>
+		public SocketAsyncEventArgs ReceiveArgs
+		{
+			get; set;
+		}
+
+		/// <summary>
+		/// 获取或设置接收数据用的SocketAsyncEventArgs
+		/// </summary>
+		public SocketAsyncEventArgs SendArgs
+		{
+			get; set;
+		}
+
+		/// <summary>
+		/// 获取缓存数据处理对象
+		/// </summary>
+		public ISocketProtocol BufferProcess
+		{
+			get;
+			private set;
+		}
+
+
+
+		/// <summary>
+		/// 初始化 SocketUserToken 实例
+		/// </summary>
+		public SocketUserToken( ISocketProtocol bufferProcess, int singleBufferMaxSize )
+		{
+			if ( bufferProcess == null )
+			{
+				throw new ArgumentNullException( "bufferProcess" );
+			}
+			
+			BufferProcess = bufferProcess;
+			bufferManager = BufferManager.CreateBufferManager( 2, singleBufferMaxSize );
+		}
+
+		
+
+		/// <summary>
+		/// 处理接收到的数据
+		/// </summary>
+		public void ProcessReceive()
+		{
+			//BufferProcess.Decoder( ReceiveArgs.Buffer, ReceiveArgs.BytesTransferred );
+		}
+
+		public void ProcessSend()
+		{
+		}
+	}
+}
